8c993ff548eacd5b4575652adf53d168f240f48b
Nickolaus D. Saint <nick@globus.org>
2021-01-26 12:27:13 -0800

    Upgrade fair-research-login to support submitting remote datasets

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "bdbag>=1.5.5",
         "Click>=7.0",
         "datapackage>=1.10.0",
-        "fair-research-login>=0.1.3",
+        "fair-research-login>=0.2.0",
         "GitPython>=3.0.4",
         "globus-automate-client==0.10.4",
         "globus-sdk>=1.8.0",
